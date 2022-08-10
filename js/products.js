51cc5148d954c4182e422d47b0f6dcf0cf4e0a29
agustin noya <108200137+Agustinn57@users.noreply.github.com>
2022-08-10 00:46:24 -0300

    products.html.js quedo funcionando el json:)

--- js/products.js
+++ js/products.js
@@ -1,17 +1,74 @@
-const url = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+//const url = "https://japceibal.github.io/emercado-api/cats_products/101.json"
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => mostrarData(data))
-    .catch(error => console.log(error))
+//fetch(url)
+//    .then(response => response.json())
+//    .then(data => mostrarData(data))
+//    .catch(error => console.log(error))
 
-const mostrarData = (data) => {
-    console.log(data)
+//const mostrarData = (data) => {
+//    console.log(data)
+//    console.log(data.products)
+//    let body = ""
+//    for (let i = 0; i < data.products.length; i++) {
+//        body +=  `${data.products[i].id}` 
+//    }
+//    document.getElementById("data").innerHTML = body
+// }
 
-    let body = ""
-    for (let i = 0; i < data.length; i++) {
-        body += "<tr><td>${data[i].id<td><tr>"
+
+
+    let htmlContentToAppend = "";
+    // for(let i = 0; i < currentCategoriesArray.length; i++){
+    //     let category = currentCategoriesArray[i];
+
+    // if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
+    //     ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
+
+
+    const url = "https://japceibal.github.io/emercado-api/cats_products/101.json"
+    console.log("holaaa") 
+    fetch(url)
+        .then((response) => response.json())
+        //.then((data) => console.log(data))
+        .then((data) => mostrarData(data))
+        .catch(error => console.log(error))
+
+    const mostrarData = (data) => {
+        console.log(data)
+        console.log(data.products)
+        //let body = ""
+        for (let i = 0; i < data.products.length; i++) {
+            htmlContentToAppend += `
+            <div onclick="setCatID(${data.products[i].id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${data.products[i].image}" alt="${data.products[i].description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${data.products[i].name}</h4>
+                            <small class="text-muted">${data.products[i].soldCountt} artículos</small>
+                        </div>
+                        <p class="mb-1">${data.products[i].description}</p>
+                    </div>
+                </div>
+            </div>
+            `
+            
+        }
+        document.getElementById("data").innerHTML = htmlContentToAppend; 
+        
+       // document.getElementById("data").innerHTML = body
     }
-    document.getElementById("data").innerHTML = body
-}
+    
+       //     document.getElementById("data").innerHTML = htmlContentToAppend;
+           
+
+ 
+
+
+
+    
+
+
 
