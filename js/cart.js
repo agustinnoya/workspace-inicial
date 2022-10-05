7b7f272d3194782fc91adbd95c5e1cafc397e796
agustin noya <108200137+Agustinn57@users.noreply.github.com>
2022-10-05 11:46:12 -0300

    semana 5
    semana 5 casi terminado

---
+++ js/cart.js
@@ -0,0 +1,59 @@
+const url_carrito = "https://japceibal.github.io/emercado-api/user_cart/" + localStorage.getItem(`usuario`) + ".json"
+console.log(url_carrito)
+let carrito
+
+
+function carrito_JSON() {
+    fetch(url_carrito)
+        .then(response => response.json())
+        .then(data => { console.log(data); return data })
+        .then(data => { carrito = data; return data })
+        .then(producto => { console.log("carrito", carrito); return carrito })
+        .then(producto => carrito_print())
+        .then(producto => carrito_subtotal_inicial())
+        .catch(error => console.log(error))
+}
+carrito_JSON();
+
+function carrito_print() {
+    console.log("dentro de carrito", carrito)
+    let data_carrito = ""
+
+    for (let i = 0; i < carrito.articles.length; i++) {
+        data_carrito += `<img class="imagen_carrito" src="${carrito.articles[i].image}"><p>${carrito.articles[i].name}</p><p>USD ${carrito.articles[i].unitCost}</p> <div><input type="number" name="cantidad" class="form-control cantidad_box" id="${i}" value="1"
+        oninput="cantidad_cuadro()"></div>`
+    }
+    document.getElementById("carrito_producto").innerHTML = data_carrito;
+    return carrito;
+}
+
+function carrito_subtotal_inicial() {
+    console.log("dentro de carrito", carrito)
+    let data_subtotal = ""
+    data_subtotal += `<strong><p>USD ${carrito.articles[0].unitCost}</p></strong>`
+    document.getElementById("carrito_subtotal").innerHTML = data_subtotal;
+    return carrito;
+}
+
+function cantidad_cuadro() {
+    for (let i = 0; i < carrito.articles.length; i++) {
+    
+    let subtotal = document.getElementById([i]).value * carrito.articles[i].unitCost
+    console.log(subtotal)
+    carrito_subtotal(subtotal)
+    }
+}
+
+function carrito_subtotal(subtotal) {
+    console.log(subtotal)
+    let data_subtotal = ""
+
+    data_subtotal += `
+        <strong><p>USD ${subtotal}</p></strong>
+            `
+
+
+    document.getElementById("carrito_subtotal").innerHTML = data_subtotal;
+
+
+}
